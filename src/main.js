111e8ff882eb97ebda1e7ddfd880f9a8f12e1560
Martin Kang <martin.kang@efrei.net>
2024-09-29 19:18:29 +0200

    Add Font Awesome packages for icons

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// Import de Vue
+import { createApp } from 'vue';
+import App from './App.vue';
+import './style.css';
 
-createApp(App).mount('#app')
+// Import de FontAwesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { far } from '@fortawesome/free-regular-svg-icons'; // Pour importer tous les icônes solides
+import { fas } from '@fortawesome/free-solid-svg-icons';
+// Ajout des icônes à la librairie
+library.add(far);
+library.add(fas);
+
+const app = createApp(App);
+
+// Déclaration du composant global FontAwesomeIcon
+app.component('font-awesome-icon', FontAwesomeIcon);
+
+app.mount('#app');
