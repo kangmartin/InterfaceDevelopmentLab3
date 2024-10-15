be88a9b0da80ac37020e7819e2b0d29bcde5bb3d
Martin Kang <martin.kang@efrei.net>
2024-10-15 13:05:24 +0200

    Revert "Ex5: add vue-router dependency"
    This reverts commit c1fc6e652503468dccc5fc7d4da8e574e5553993.

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -18,11 +18,11 @@ export default {
     <div class="left-container">
       <font-awesome-icon :icon="['fas', 'house']" class="icon"/>
       <a href="index.html" class="logo">Home</a>
-      <router-link v-if="user" to="/conversations">Conversations</router-link>
     </div>
     <SigninButton />
   </div>
 </template>
+
 <style scoped>
 
   @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
